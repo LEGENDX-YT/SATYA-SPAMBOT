00459a8fea1ef60a1e50c59f681669cc48bf5829
【★༒✞︎©༒SATYA-OP】MR-SATYA-OP <104726886+SATYA-OP@users.noreply.github.com>
2022-05-04 07:23:26 -0700

    Create spammer.js

---
+++ spammer.js
@@ -0,0 +1,16 @@
+FROM github.com/LegendSatya.git/root/spammerbots
+RUN  javaspammers , pythonspammers , linuxspammers .
+legendspambot== satya_spammer()
+SATYA()
+       try:
+             running_spambot_msh=(1000kms.speed_miters__.inti.user(int("SpamBot_Satya_Bot"))).utils.clintes
+             running_userbot_msh==(100kms.speed_Hang_telegram.clints(10min.>))int.users.py
+             running_speed(1409kms par in(await.user_hang).utils.telegramclints_leck
+        try():
+ 
+       user.spam,hackers()await(input_speedo_spammers_team).get(str({&^76900.&^}None,)).speed.time()10min
+  pass::output==None,
+  HANG_TELEGRAM_UTILE_PYTHON_JAVA_LINUX,hang>
+  FULL_HANG_OWNER
+  LEGG().
+  
